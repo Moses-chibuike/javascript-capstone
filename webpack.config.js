69e155569417a4648b704e4afaca2195a2335bb3
theophilus doku <theodoku89@gmail.com>
2023-03-14 15:23:35 +0000

    modified webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -13,10 +13,6 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
     ],
   },
   plugins: [

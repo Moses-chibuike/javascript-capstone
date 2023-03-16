df556cf0eec024ec323e876797e2f1f75b11727c
theophilus doku <theodoku89@gmail.com>
2023-03-16 18:52:30 +0000

    fixed linter error for eslint

--- src/index.js
+++ src/index.js
@@ -6,16 +6,12 @@ const gallery = document.querySelector('.gallery');
 loadData();
 
 gallery.addEventListener('click', (event) => {
-    const { target } = event;
-    if (target.value === 'Comments') {
-      openComments(target.id);
-    } else if (target.name === 'heart') {
-      const idlike = target.id;
-      const idlikefiltered = idlike.replace(/^\D+/g, '');
-      likeArts(idlikefiltered);
-      let numlikes = Number(target.alt.replace(/^\D+/g, ''));
-      numlikes += 1;
-      const likecontainer = document.getElementById(`spl${idlikefiltered}`);
-      likecontainer.innerHTML = `Likes(${numlikes})`;
-    }
-  });
+  const { target } = event;
+  const idlike = target.id;
+  const idlikefiltered = idlike.replace(/^\D+/g, '');
+  likeArts(idlikefiltered);
+  let numlikes = Number(target.alt.replace(/^\D+/g, ''));
+  numlikes += 1;
+  const likecontainer = document.getElementById(`spl${idlikefiltered}`);
+  likecontainer.innerHTML = `Likes(${numlikes})`;
+});

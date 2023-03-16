851eae0142789fca509e29e4ee834bbef63add72
theophilus doku <theodoku89@gmail.com>
2023-03-16 18:31:44 +0000

    modified style.css, index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 import './style.css';
 import loadData from './modules/Dom.js';
+import { likeArts } from './modules/api.js';
 
+const gallery = document.querySelector('.gallery');
 loadData();
+
+gallery.addEventListener('click', (event) => {
+    const { target } = event;
+    if (target.value === 'Comments') {
+      openComments(target.id);
+    } else if (target.name === 'heart') {
+      const idlike = target.id;
+      const idlikefiltered = idlike.replace(/^\D+/g, '');
+      likeArts(idlikefiltered);
+      let numlikes = Number(target.alt.replace(/^\D+/g, ''));
+      numlikes += 1;
+      const likecontainer = document.getElementById(`spl${idlikefiltered}`);
+      likecontainer.innerHTML = `Likes(${numlikes})`;
+    }
+  });
